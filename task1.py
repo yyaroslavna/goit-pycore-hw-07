f84f1bd08c050ec16f12f3b0249ed79071b23a48
yyaroslavna <yaroslavna1008@gmail.com>
2026-08-18 10:14:36 +0200

    Update

--- task1.py
+++ task1.py
@@ -1,3 +1,100 @@
+from collections import UserDict
+from datetime import datetime, timedelta
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+		pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) == 10 and value.isdigit():
+            self.value = value
+        else:
+            raise ValueError("Invalid phone number")
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            birthday = datetime.strptime(value, "%d.%m.%Y")
+            self.value = birthday 
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def remove_phone(self, phone_number):
+        phone = self.find_phone(phone_number)
+        if phone:
+            self.phones.remove(phone)
+
+    def edit_phone(self, old_phone, new_phone):
+        phone = self.find_phone(old_phone)
+        if phone:
+            update_phone = Phone(new_phone)
+            self.phones.remove(phone)
+            self.phones.append(update_phone)
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+           return self.data[name]
+        return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def get_upcoming_birthdays(self):
+        today = datetime.today().date()
+        next_week = today + timedelta(days=7)
+        upcoming = []
+        for record in self.data.values():
+            if record.birthday:
+                birthday = record.birthday.value
+                birthday_this_year = birthday.replace(year=today.year).date()
+                if birthday_this_year < today:
+                    birthday_this_year = birthday.replace(year=today.year + 1).date()
+                if today <= birthday_this_year <= next_week:
+                    if birthday_this_year.weekday() == 5:
+                        birthday_this_year += timedelta(days=2)
+                    elif birthday_this_year.weekday() == 6:
+                        birthday_this_year += timedelta(days=1)
+                    upcoming.append({
+                        "name": record.name.value,
+                        "birthday": birthday_this_year.strftime("%d.%m.%Y")
+                    })
+        return upcoming
+
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
@@ -118,104 +215,6 @@ def main():
             print(birthdays(args, book))
         else:
             print("Invalid command.")
-
-
-
-
-from collections import UserDict
-from datetime import datetime, timedelta
-
-class Field:
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return str(self.value)
-
-class Name(Field):
-		pass
-
-class Phone(Field):
-    def __init__(self, value):
-        if len(value) == 10 and value.isdigit():
-            self.value = value
-        else:
-            raise ValueError("Invalid phone number")
-
-class Birthday(Field):
-    def __init__(self, value):
-        try:
-            birthday = datetime.strptime(value, "%d.%m.%Y")
-            self.value = birthday 
-        except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY")
-
-class Record:
-    def __init__(self, name):
-        self.name = Name(name)
-        self.phones = []
-        self.birthday = None
-
-    def add_phone(self, phone):
-        self.phones.append(Phone(phone))
-
-    def find_phone(self, phone_number):
-        for phone in self.phones:
-            if phone.value == phone_number:
-                return phone
-        return None
-
-    def remove_phone(self, phone_number):
-        phone = self.find_phone(phone_number)
-        if phone:
-            self.phones.remove(phone)
-
-    def edit_phone(self, old_phone, new_phone):
-        phone = self.find_phone(old_phone)
-        if phone:
-            update_phone = Phone(new_phone)
-            self.phones.remove(phone)
-            self.phones.append(update_phone)
-
-    def add_birthday(self, birthday):
-        self.birthday = Birthday(birthday)
-
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def find(self, name):
-        if name in self.data:
-           return self.data[name]
-        return None
-
-    def delete(self, name):
-        if name in self.data:
-            del self.data[name]
-
-    def get_upcoming_birthdays(self):
-        today = datetime.today().date()
-        next_week = today + timedelta(days=7)
-        upcoming = []
-        for record in self.data.values():
-            if record.birthday:
-                birthday = record.birthday.value
-                birthday_this_year = birthday.replace(year=today.year).date()
-                if birthday_this_year < today:
-                    birthday_this_year = birthday.replace(year=today.year + 1).date()
-                if today <= birthday_this_year <= next_week:
-                    if birthday_this_year.weekday() == 5:
-                        birthday_this_year += timedelta(days=2)
-                    elif birthday_this_year.weekday() == 6:
-                        birthday_this_year += timedelta(days=1)
-                    upcoming.append({
-                        "name": record.name.value,
-                        "birthday": birthday_this_year.strftime("%d.%m.%Y")
-                    })
-        return upcoming
 
 if __name__ == "__main__":
     main()
